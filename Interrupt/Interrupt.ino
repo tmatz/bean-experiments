96f2f9ed4a92e2ed96ae4e71211ef36b8ff57ee2
MATSUDA Takashi <t.matsuda.1000@gmail.com>
2016-10-04 02:55:42 +0900

    interrupt

--- Interrupt/Interrupt.ino
+++ Interrupt/Interrupt.ino
@@ -1,22 +1,50 @@
-#define BUTTON 12
+uint8_t sPciNo[] = {
+  18, 20, 22, 23, 0, 1, 2, 3, 4, 5, // D0-9
+  8, 9, 10, 11, 12, 13 // A0-5
+};
+
+void setInterrupt(uint8_t pin) {
+  if (pin >= sizeof(sPciNo)) {
+    return;
+  }
+  uint8_t pciNo = sPciNo[pin];
+  uint8_t group = pciNo / 8;
+  PCICR |= _BV(group);
+  *(&PCMSK0 + group) |= _BV(pciNo % 8);
+}
+
+boolean a3 = false;
+boolean a3Changed = false;
 
 void setup() {
-   pinMode(BUTTON, INPUT);
-   attachInterrupt(
-       digitalPinToInterrupt(BUTTON), pinChanged, CHANGE);
+  pinMode(A3, INPUT);
+  setInterrupt(A3);
+  a3 = digitalRead(A3);
+  setLed(a3);
 }
 
 void loop() {
-  Bean.sleep(0xFFFFFFFF);
-  bool val = digitalRead(BUTTON);
-  if (val) {
-     Bean.setLed(255, 0, 0);
-  } else {
-     Bean.setLed(0, 255, 0);
-  }
-  Bean.sleep(100);
-  Bean.setLed(0, 0, 0);
+    Bean.sleep(0xFFFFFFFF);
+    setLed(a3);
 }
 
-void pinChanged() {
+void setLed(boolean b) {
+  if (b) {
+    Bean.setLed(0,30,0);
+  } else {
+    Bean.setLed(0,0,0);
+  }
+}
+
+ISR(PCINT0_vect) {
+}
+
+ISR(PCINT1_vect) {
+  bool old = a3;
+  a3 = digitalRead(A3);
+  a3Changed = a3 != old;
+}
+
+
+ISR(PCINT2_vect) {
 }
